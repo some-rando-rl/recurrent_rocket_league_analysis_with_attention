7908182c3c44d571f6621bbbe6ce8de853125f7d
Matej <matej.novosad.mn@gmail.com>
2022-04-23 22:41:20 +0200

    added validation code

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import torch
-
+import wandb
 from model import NextGoalPredictor
 from replay_processing.utils import get_all_bins, get_batch
 from torch import nn, optim
+
+# import wandb
+#
+# wandb.init(project="next-goal-predictor", entity="mrkvicka02")
 
 
 def train_loop(model, loss_fn, optimizer, save_dir, epochs=1, device=torch.device("cpu"), save_frequency=50):
     model.to(device)
     for epoch in range(epochs):
-        bins = get_all_bins("replay_processing/bins")
+        bins = get_all_bins("replay_processing/bins", 900)
         epoch_loss = 0
         for i, file_names in enumerate(bins):
             batch_arr = get_batch(file_names)
@@ -28,8 +32,24 @@ def train_loop(model, loss_fn, optimizer, save_dir, epochs=1, device=torch.devic
         print(f"Epoch {epoch} done with loss of {epoch_loss}")
 
 
+def validation_run(model, loss_fn, device):
+    model.to(device)
+    with torch.no_grad():
+        validation_loss, correct, all = 0, 0, 0
+        bins = get_all_bins("replay_processing/validation_bins", 90)
+        for i, file_names in enumerate(bins):
+            batch_arr = get_batch(file_names)
+            inputs, labels = batch_arr[0].to(device), batch_arr[1].to(device)
+            # Compute prediction and loss
+            output = model(inputs)
+            validation_loss += loss_fn(output, labels).item()
+            correct += (output.argmax(1) == labels).type(torch.float).sum().item()
+            all += len(labels)
+    return validation_loss, correct / all
+
+
 if __name__ == '__main__':
     model = NextGoalPredictor()
     optimizer = optim.Adam(model.parameters(), lr=3e-4)
     loss_funciton = nn.CrossEntropyLoss(reduction="mean")
-    train_loop(model, loss_funciton, optimizer,"models",epochs=2, device=torch.device("cuda"))
+    train_loop(model, loss_funciton, optimizer, "models", epochs=10, device=torch.device("cuda"))
