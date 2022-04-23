bd7a2d00ae5ea4b0a04a3741eeddf7a3f0e8296a
Matej <matej.novosad.mn@gmail.com>
2022-04-23 20:01:32 +0200

    added saving frequency

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from model import NextGoalPredictor
 from replay_processing.utils import get_all_bins, get_batch
 from torch import nn, optim
 
-def train_loop(model, loss_fn, optimizer, device, epochs):
+
+def train_loop(model, loss_fn, optimizer, epochs, save_dir, device="cuda", save_frequency=50):
     model.to(device)
     for epoch in range(epochs):
         bins = get_all_bins("replay_processing/bins")
@@ -20,9 +21,12 @@ def train_loop(model, loss_fn, optimizer, device, epochs):
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            epoch_loss+=loss.item()
+            epoch_loss += loss.item()
             print(f"Batch:{i}; Len batch: {len(inputs)}; Loss:{loss.item()}")
+            if i % save_frequency == save_frequency - 1:
+                torch.save(model.state_dict(), f"{save_dir}/model_{epoch}_{i}.pckl")
         print(f"Epoch {epoch} done with loss of {epoch_loss}")
+
 
 if __name__ == '__main__':
     model = NextGoalPredictor()
